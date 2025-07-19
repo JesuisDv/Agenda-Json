e79d585b9d7a7add1663c90c21e43de3a28dc669
Jesús Franco <jdfrancov2020@gmail.com>
2025-07-19 18:45:44 -0500

    Refactor task management: update task completion logic and improve error handling

--- src/main.js
+++ src/main.js
@@ -1,37 +1,64 @@
 const apiURL = "http://localhost:3000/tareas";
 
 const form = document.querySelector("#formData");
-const list = document.querySelector("#tasks");
+const list = document.getElementById("tasks");
 const input = document.querySelector("#date");
 
 async function getData() {
-
-  try{
+  try {
     const response = await fetch(apiURL);
     const data = await response.json();
     console.log(data);
 
-  }catch{
-    console.error("Error trayendo los datos: ", error)
+    list.innerHTML = "";
 
+    data.forEach((item) => {
+      const li = document.createElement("li");
+      li.innerHTML = `<input type="checkbox" ${item.completada ? 'checked' : ''} onchange="toggleEstado(${item.id}, this.checked) "> ${item.titulo}`;
+      list.appendChild(li);
+    });
+
+    
+
+  } catch {
+    console.error("Error trayendo los datos: ", error);
   }
-};
+}
 
 // Headers y body
 form.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const newObject = {
-      id: Date.now(),
-      titulo: input.value,
-      completada: false
-    };
+  e.preventDefault();
+  const newObject = {
+    id: String(Date.now()),
+    titulo: input.value,
+    completada: false,
+  };
 
-    fetch(apiURL,{
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(newObject)
-    });
-    getData();
+  fetch(apiURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newObject),
+  });
+  getData();
 });
+
+// Actualizar checkboxes
+async function toggleEstado(id, estado) {
+  try{
+    const response = fetch(`${apiURL}/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ completada: estado }),
+    })
+  }catch (error) {
+    console.error("Error actualizando el estado: ", error);
+  }
+  getData();
+
+};
+
+// Filtrar tareas por completadas, pendientes o todas
