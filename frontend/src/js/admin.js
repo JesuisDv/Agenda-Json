51da89d3e70c7e392acfe260b50720ec39735cf8
Jesús Franco <jdfrancov2020@gmail.com>
2025-12-30 15:24:27 -0500

    Render de citas en el dashboard fron listo y funcioanndo

--- frontend/src/js/admin.js
+++ frontend/src/js/admin.js
@@ -1,5 +1,65 @@
+// ===============================
+// Protección del dashboard
+// ===============================
 const token = localStorage.getItem('token')
 
 if (!token) {
     window.location.href = '/admin.html'
 }
+
+// ===============================
+// Obtener citas desde el backend
+// ===============================
+
+async function loadAppointments() {
+    try{
+        const res = await fetch('http://localhost:3000/api/appointments', {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+
+        const data = await res.json()
+
+        if (!res.ok){
+            throw new Error(data.error || 'Error al cargar citas')
+        }
+
+        renderAppointments(data.data)
+
+    }catch(error){
+        console.error(error)
+        alert('Error cargando citas')
+    }
+}
+
+// ===============================
+// Pintar citas en el HTML
+// ===============================
+
+function renderAppointments(appointments){
+    const container = document.getElementById('appointmentsList')
+    container.innerHTML = ''
+
+    if(appointments.length === 0){
+        container.innerHTML = '<p>No Hay Citas</p>'
+        return
+    }
+
+    appointments.forEach(app =>{
+        const div = document.createElement('div')
+        div.classList.add('appointment')
+
+        div.innerHTML = `
+            <strong>${app.customer_name}</strong><br>
+            📞 ${app.customer_phone}<br>
+            📅 ${app.appointment_date} ⏰ ${app.appointment_time}<br>
+            🏷 Estado: ${app.status}
+        `
+
+        container.appendChild(div)
+    })
+}
+
+//Se ejecuta la carga en el dashboard
+loadAppointments()
