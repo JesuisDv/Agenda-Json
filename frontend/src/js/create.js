b06fb1307e0c6a732f790b3fec08aae3ce3259e5
Jesús Franco <jdfrancov2020@gmail.com>
2026-02-20 22:20:48 -0500

    Agregando funcionalidad de creacion de citas

--- frontend/src/js/create.js
+++ frontend/src/js/create.js
@@ -4,15 +4,15 @@ const message = document.getElementById('message')
 form.addEventListener('submit', async (e) => {
   e.preventDefault()
 
-  const appointment = {
-    client_name: document.getElementById('client_name').value,
-    client_phone: document.getElementById('client_phone').value,
-    appointment_date: document.getElementById('appointment_date').value,
-    appointment_time: document.getElementById('appointment_time').value,
-  }
+const appointment = {
+  customer_name: document.getElementById('client_name').value,
+  customer_phone: document.getElementById('client_phone').value,
+  appointment_date: document.getElementById('appointment_date').value,
+  appointment_time: document.getElementById('appointment_time').value,
+}
 
   try {
-    const res = await fetch('https://TU-URL/api/appointments', {
+    const res = await fetch('http://localhost:3000/api/appointments', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(appointment)
